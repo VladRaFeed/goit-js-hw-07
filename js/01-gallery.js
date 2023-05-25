27d38e93e102c5f772742dbdca3517a0d19a21ac
unknown <jack22toplolkek@gmail.com>
2023-05-25 22:20:28 +0300

    Competed all tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryObj = document.querySelector(".gallery");
 
-console.log(galleryItems);
+galleryObj.addEventListener("click", onGalleryContainerClick);
+
+const imagesMarkup = createImageItemMarkup(galleryItems);
+
+galleryObj.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createImageItemMarkup(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+        `;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const largeImgSource = e.target.dataset.source;
+
+  const modalImg = `
+        <img src = '${largeImgSource}', alt = 'LargeImage'>
+    `;
+
+  const instanceModal = basicLightbox.create(modalImg);
+
+  instanceModal.show();
+}
